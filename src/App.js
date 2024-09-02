6942577f181e67e2dd5fd7270b4d300a01b8b190
skhreis <skhreis0@gmail.com>
2024-09-02 22:51:53 +0300

    Sure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 function App() {
+  const [pic,setPic] = useState('https://i.ibb.co/zXhQ2Sz/red-traffic-light.png')
+  const [activity, setActivity] = useState('Not Active')
+  useEffect(() => {
+  }, [pic]) 
+  
+  function start_timer(){
+    setPic('https://i.ibb.co/zXhQ2Sz/red-traffic-light.png')
+    setActivity('Active')
+    setTimeout(() =>{
+      set_green()
+    }, 5000)
+  }
+
+  function set_green(){
+    setPic('https://i.ibb.co/6yTfSHg/green-traffic-light.png')
+    console.log('green')
+      setTimeout(() =>{
+        set_yellow()
+      }, 5000)
+  }
+  function set_yellow(){
+    setPic('https://i.ibb.co/fqc9D2M/yellow-traffic-light.png')
+    console.log('yellow')
+      setTimeout(() =>{
+        set_red()
+      }, 3000)
+  }
+  function set_red(){
+    setPic('https://i.ibb.co/zXhQ2Sz/red-traffic-light.png')
+    console.log('red')
+      setTimeout(() =>{
+      set_green()
+    }, 5000)
+  }
+
+  function stop_timer(){
+    window.location.reload(false);
+    setActivity('Not Active')
+    setPic('https://i.ibb.co/zXhQ2Sz/red-traffic-light.png')
+  }
+
+  function override(e){
+    e.preventDefault()
+    console.log(e)
+    const value = e.target.value
+    console.log(value)
+    if(value === 'red'){
+      setPic('https://i.ibb.co/zXhQ2Sz/red-traffic-light.png')
+    } 
+    if(value === 'yellow'){
+      setPic('https://i.ibb.co/fqc9D2M/yellow-traffic-light.png')
+    }
+    if(value === 'green'){
+      setPic('https://i.ibb.co/6yTfSHg/green-traffic-light.png')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="traffic-light">
+      <img class="color" src ={pic}/>
     </div>
-  );
-}
+    <div>
+        <button onClick={start_timer} class="start">Start Timer</button>
+        <button onClick={stop_timer} class="stop">Stop Timer</button>
+        <div>
+            Manual Override: <select onChange={override} name="Manual Override" class="selection">
+                <option value="red">Red</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+            </select>
+        </div>
+    </div>
+    <div class="activity">Timer is {activity}</div>
+    </div>
+)};
 
 export default App;
